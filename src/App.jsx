697f35a5538d28c3213a3bd524d7ee242cb686fd
Promise-Digitals <promiseedeh025@gmail.com>
2024-08-26 00:02:33 +0100

    updated the UI design

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Weather from "./Components/Weather";
+import './App.css'
 
 const App = () => {
   return(
     <div className="app">
+      <img src={('assets/logo.png')} alt="" className="logo"/>
       <Weather />
     </div>
   )
